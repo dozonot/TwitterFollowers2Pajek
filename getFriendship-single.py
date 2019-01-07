aa65de3d877db65d04c8c74453ce34cb03f5984e
Dozono <dozono@dozono.net>
2019-01-07 20:04:24 +0900

    Add getFriendship-single.py

---
+++ getFriendship-single.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+import sys, json, config, argparse
+from retry import retry
+from requests_oauthlib import OAuth1Session
+
+def auth():
+    CK  = config.CONSUMER_KEY
+    CS  = config.CONSUMER_SECRET
+    AT  = config.ACCESS_TOKEN
+    ATS = config.ACCESS_TOKEN_SECRET
+    return OAuth1Session(CK, CS, AT, ATS)
+
+@retry(tries=3, delay=900)
+def request_twitter_api(api, url, params):
+    try:
+        print("Request Twitter APIs")
+        res = api.get(url, params = params)
+        if res.status_code == 200:
+            print("Success.")
+            return res
+        elif res.status_code == 429:
+            print("Error: {}, Too many requests".format(res.status_code))
+            print("This script sleeps for 900 seconds and then retries.")
+            raise TooManyRequests('Too many requests Twitter APIs')
+        else:
+            print("Error: {}".format(res.status_code))
+            sys.exit()
+    except TooManyRequests as e:
+        print(type(e))
+
+def find_all_followers(api, user):
+    url    = "https://api.twitter.com/1.1/followers/list.json"
+    cursor = -1
+    followers_dict = {}
+    followers_dict[user] = []
+    while True:
+        params = {
+            'screen_name' : user,
+            'count'       : 200,
+            'cursor'      : cursor,
+            'skip_status' : 'true'
+            }
+        res = request_twitter_api(api, url, params)
+        text = json.loads(res.text)
+        for follower in text['users']:
+            followers_dict[user].append(follower['screen_name'])
+        
+        cursor = text['next_cursor']
+        if cursor == 0:
+            return followers_dict
+
+def find_all_friends(api, user):
+    url    = "https://api.twitter.com/1.1/friends/list.json"
+    cursor = -1
+    friends_dict = {}
+    friends_dict[user] = []
+    while True:
+        params = {
+            'screen_name' : user,
+            'count'       : 200,
+            'cursor'      : cursor,
+            'skip_status' : 'true'
+            }
+        res = request_twitter_api(api, url, params)
+        text = json.loads(res.text)
+        for friend in text['users']:
+            friends_dict[user].append(friend['screen_name'])
+        
+        cursor = text['next_cursor']
+        if cursor == 0:
+            return friends_dict
+
+def merge_ff(followers_list, friends_list):
+    merge_list = [user]
+    merge_list = merge_list + followers_list
+    for friend in friends_list:
+        if friend in followers_list:
+            continue
+        else:
+            merge_list.append(friend)
+    return merge_list
+
+def create_friendship(followers_dict, friends_dict, user):
+    with open("friendships.net", mode='w') as f:
+        # Add Vertice record.
+        merge_list = merge_ff(followers_dict[user], friends_dict[user])
+        f.write('*Vertices {}\n1 "{}"\n'.format(len(merge_list), user))
+        vertice = 2
+
+        for follower in followers_dict[user]:
+            f.write('{} "{}"\n'.format(vertice, follower))
+            vertice += 1
+
+        for friend in friends_dict[user]:
+            # Add only those who do not duplicate.
+            if friend in followers_dict[user]:
+                continue
+            else:
+                f.write('{} "{}"\n'.format(vertice, friend))
+                vertice += 1
+        
+        # Add Arcs record.
+        f.write("*Arcs\n")
+        for follower in followers_dict[user]:
+            arc = merge_list.index(follower) + 1
+            f.write('{} 1\n'.format(arc))
+        for friend in friends_dict[user]:
+            arc = merge_list.index(friend) + 1
+            f.write('1 {}\n'.format(arc))
+    print("friendships.net created")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+      prog="getFriendship.py",
+      description='Get followers and friends, and generate friendships.net file.'
+    )
+      
+    parser.add_argument("-u",
+                        "--user",
+                        type=str,
+                        required=True,
+                        help='<Required> Twitter user name(screen_name) to acquire. (example : @dozonot => "dozonot")'
+    )
+
+    args  = parser.parse_args()
+    user  = args.user
+    api   = auth()
+    followers_dict = {}
+    friends_dict   = {}
+
+    followers_dict.update(find_all_followers(api, user))
+    friends_dict.update(find_all_friends(api, user))
+    create_friendship(followers_dict, friends_dict, user)
